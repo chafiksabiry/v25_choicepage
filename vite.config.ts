d08a8eeac5db6eccc140d79bb8eb58ac93fd5f77
hosam02 <houssam.qotbi@esi.ac.ma>
2025-03-13 11:56:17 +0000

    possible solution

--- vite.config.ts
+++ vite.config.ts
@@ -37,12 +37,14 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 5173,
-      cors: {
+      /*cors: {
         origin: 'https://v25.harx.ai', // The domain from which you want to allow CORS requests
         methods: ['GET', 'POST', 'OPTIONS'], // Allowed HTTP methods
         allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
         credentials: true, // If you need to send credentials (cookies, HTTP authentication, etc.)
-      },hmr: false,
+      },*/
+      cors: false,
+      hmr: false,
       fs: {
         strict: true, // Ensure static assets are correctly resolved
       },
