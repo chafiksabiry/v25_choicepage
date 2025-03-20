77db50e6a8b8fc02f0c65092835b3bbfe37d8214
hosam02 <houssam.qotbi@esi.ac.ma>
2025-03-20 12:37:40 +0000

    updated the base

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: './',
+    base: '/',
     plugins: [
       react({
         jsxRuntime: 'classic',
